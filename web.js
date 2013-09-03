698bb7285f25b8e5a80de3a5e60cb2f3a9373682
Seb Insua <me@sebinsua.com>
2013-09-03 16:16:51 +0100

    Sensible structure.

--- web.js
+++ web.js
@@ -1,9 +1,23 @@
-var express = require("express");
+var express = require("express"),
+    stylus = require("stylus"),
+    nib = require("nib");
+
 var app = express();
 app.use(express.logger());
+app.use(express.static(__dirname + "/public"));
+app.use("view engine", "jade");
+app.use(stylus.middleware(
+    {
+        "src": __dirname + "/public",
+        "compile": function compile(str, path) {
+            return stylus(str).set("filename", path).use(nib())
+        }
+    }
+));
+app.set("views", __dirname + "/views");
 
 app.get("/", function (req, res) {
-    res.send("Hello World!");
+    res.end("Hello World!");
 });
 
 var port = process.env.PORT || 5000;
