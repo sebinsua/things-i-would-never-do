caca71c57f347d5aaf46e837eba525e21611366b
Seb Insua <me@sebinsua.com>
2013-09-12 00:25:04 +0100

    Jslint installed but not being run. Just from Sublime... :)

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 test:
-	./node_modules/.bin/mocha --reporter list
+	./node_modules/.bin/mocha test/server --reporter list &&\
+	./node_modules/.bin/karma start --basePath test/client --single-run --browsers Chrome
 
 .PHONY: test
---
+++ test/server/sanity.js
@@ -0,0 +1,10 @@
+var chai = require("chai");
+var expect = chai.expect;
+
+describe("SomeModule", function () {
+    describe("someMethod()", function () {
+        it("should always pass", function () {
+            expect(1).to.equal(1);
+        });
+    })
+})
